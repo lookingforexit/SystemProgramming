ad265ed365282e17730586fa1c4ea3471f12853f
lookingforexit <lookingforexit@yandex.com>
2026-04-28 12:43:40 +0300

    fix gh alloc

--- allocator/allocator_global_heap/src/allocator_global_heap.cpp
+++ allocator/allocator_global_heap/src/allocator_global_heap.cpp
@@ -1,25 +1,18 @@
-#include <not_implemented.h>
 #include "../include/allocator_global_heap.h"
 
-#include <mutex>
-
-
-[[nodiscard]] void *allocator_global_heap::do_allocate_sm(
-    size_t size)
+[[nodiscard]] void* allocator_global_heap::do_allocate_sm(size_t size)
 {
     std::lock_guard lock(mtx);
     return operator new(size);
 }
 
-void allocator_global_heap::do_deallocate_sm(
-    void *at)
+void allocator_global_heap::do_deallocate_sm(void* at)
 {
     std::lock_guard lock(mtx);
     operator delete(at);
 }
 
-bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource &other) const noexcept
+bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource& other) const noexcept
 {
-    auto rhs = dynamic_cast<const allocator_global_heap*>(&other);
-    return rhs != nullptr && rhs == this;
+    return this == &other;
 }
